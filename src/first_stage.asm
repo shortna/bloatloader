5b91afe03211fc60f032b9c32ca6651f5f2398b1
Dmytro Katunin <katunin_git@proton.me>
2023-12-29 15:38:43 +0200

    make first_stage report error and launch second_stage

--- src/first_stage.asm
+++ src/first_stage.asm
@@ -8,7 +8,6 @@
 _start:
   clc           # clear CF (carry flag)
   xorw %ax, %ax # set ax to zero 
-
   movb $0x02, %ah # read sector
   movb $0x01, %al # number of sectors
 
@@ -20,25 +19,87 @@ _start:
   movw $0x7E00, %bx # start of 480.5K of usable memory (https://wiki.osdev.org/Memory_Map_(x86))
   int $0x13
 
-  jc res # jump if CF (carry flag) = 1 which indicates an error
   cmp $0, %ah 
   jne res # jump if ah != 0 which indicates an error
 
-  movw $0, %cx # set counter to 0
-loop:
-  movb $0x0E, %ah # print interrupt
-  movb (%bx), %al # get symbol from buffer
-  int $0x10       # print
+  cmp $0, %al
+  je res  # jump if nothing was read
 
-  incw %cx
-  incw %bx
+  jmp *%bx # jump to second stage
 
-  cmpw $512, %cx
-  jne loop
-  
 res:
+  movb %ah, %bl  # saves error code
+  movw $err, %si # move message address to si
+
+  # print message till \0
+  message:
+    movb $0x0E, %ah
+    movb (%si), %al
+    int $0x10
+
+    incw %si
+    cmpb $0x0, (%si)
+  jne message
+
+  # prints error_code
+  pushw %bx
+  call print_byte_in_hex
+  
+  # infinite loop
   jmp .
 
-  .org 510 - _start
-  .word 0xaa55
-.ascii "Hello, World!"
+# byte to print must be located in lower half
+print_byte_in_hex:
+  pushw %bp
+  movw %sp, %bp
+
+  movb 4(%bp), %al
+  shrb $0x4, %al # shift higher 4 bits to lower 4
+
+  pushw %ax
+  call half_bit_value_to_ascii
+
+  movb $0x0E, %ah # print hex digit
+  int $0x10
+
+  movb 4(%bp), %al
+  andb $0xF, %al # remove higher 4 bits
+
+
+  pushw %ax
+  call half_bit_value_to_ascii
+
+  movb $0x0E, %ah # print hex digit
+  int $0x10
+ 
+  addw $0x4, %sp # move stack pointer 2 values back
+  popw %bp
+  ret
+
+# argument is lower 4 bits of lower byte of register
+# return is in %al
+half_bit_value_to_ascii:
+  pushw %bp
+  movw %sp, %bp
+
+  movb 4(%bp), %al
+  cmpb $0x9, %al # check if value is digit or char
+  jle number
+
+  char:
+  addb $0x37, %al # add 0x37 to make value to ascii char
+  jmp end
+
+  number:
+  addb $'0', %al # add '0' to char to make it ascii digit
+  jmp end
+
+  end:
+  popw %bp
+  ret
+
+err:
+  .ascii "Something went wrong, error code: \0"
+  
+  .org 510 - _start # pad rest wiht zeros
+  .word 0xaa55      # magic number
---
+++ src/second_stage.asm
@@ -0,0 +1,9 @@
+.code32
+
+.text
+  .global _start
+
+_start:
+  clc           # clear CF (carry flag)
+  xorw %ax, %ax # set ax to zero 
+
